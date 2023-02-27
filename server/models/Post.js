d43a1854e6cca649bf6cc1e9e6ba31ff90e7a370
rutujp78 <panbuderutuj6@gmail.com>
2023-02-27 22:12:07 +0530

    first commit

---
+++ server/models/Post.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const postSch = mongoose.Schema({
+    userId: { type: String, required: true},
+    firstName: { type: String, required: true},
+    lastName: { type: String, required: true},
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+        type: Map,
+        of: Boolean
+    },
+    comments: {
+        type: Array,
+        default: []
+    }
+}, {timestamps: true});
+
+const Post = mongoose.model("Post", postSch);
+
+export default Post;
